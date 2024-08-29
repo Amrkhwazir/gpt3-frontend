4e0a5470a3c2c1e564543a0ae34d04659c897177
Amrkhwazir <amrkh5678@gmail.com>
2024-08-29 14:08:01 +0500

    navbar working

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,14 +29,18 @@ const Navbar = () => {
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type='button'>Sign up</button>
-      </div>
+      </div>  
       <div className="gpt3__navbar-menu">
-        {toggleMenu ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(true)}/> 
-        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(false)} />}
+        {toggleMenu ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/> 
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>  
             </div>
           </div>
         )}
